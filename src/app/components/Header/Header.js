4893e6294fb9a941f2979aacb63ac0a05ba05dcc
Sandeshbelbase <sandeshbelbase>
2025-07-10 21:28:40 +1000

    structure

---
+++ src/app/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Header.module.css"
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <p style={{color: "000"}}>Header</p>
+    </div>
+  )
+}
+
+export default Header
